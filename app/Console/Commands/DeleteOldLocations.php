7cc7660c548659229c1d5354942e16d465651206
Robin Tijou <robintijou05@gmail.com>
2026-04-16 12:27:35 +0200

    ajout de la suppression des location vielle et non upvotes

---
+++ app/Console/Commands/DeleteOldLocations.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Location;
+
+class DeleteOldLocations extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'locations:delete-old';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Supprimer les locations créées depuis 14+ jours avec < 2 upvotes';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $deleted = Location::where('created_at', '<', now()->subDays(14))
+        ->where('upvotes_count', '<', 2)
+        ->delete();
+        $this->info("$deleted locations supprimées.");
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Planifier la suppression des vieilles locations
+Schedule::command('locations:delete-old')->everyMinute();
+
